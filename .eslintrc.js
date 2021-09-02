1fa3383a5a86f4313824138351bd076ef39d6520
Oleksandr orlov <orlovaleksandr89@gmail.com>
2021-09-02 11:15:10 -0700

    adding eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['plugin:react/recommended', 'standard'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: ['react'],
+  rules: {
+    indent: ['error', 2],
+    semi: ['error', 'never'],
+    quotes: ['error', 'single', { allowTemplateLiterals: true }],
+    'space-before-function-paren': ['error', 'never'],
+    'multiline-ternary': ['error', 'always-multiline']
+  }
+}
